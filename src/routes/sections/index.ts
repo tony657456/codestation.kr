2577ba27b0d7b11ba8eb85d4a945c8cbec38052e
on-stu <minsu0523@naver.com>
2023-12-05 16:54:01 +0900

    Add new components and update animation in UnderArrow.svelte

---
+++ src/routes/sections/index.ts
@@ -0,0 +1,5 @@
+export { default as IntroSection } from './intro/IntroSection.svelte';
+export { default as FlowSection } from './flow/FlowSection.svelte';
+export { default as DesignSection } from './design/DesignSection.svelte';
+export { default as WhySection } from './why/WhySection.svelte';
+export { default as StackSection } from './stacks/StackSection.svelte';
--- src/routes/sections/intro/UnderArrow.svelte
+++ src/routes/sections/intro/UnderArrow.svelte
@@ -2,6 +2,6 @@
 	export let hidden = false;
 </script>
 
-<div class="py-14 max-md:py-6 {hidden && 'opacity-0'}">
+<div class="py-14 max-md:py-6 {hidden && 'opacity-0'} animate-bounce">
 	<img src="UnderArrow.png" alt="underarrow" class="w-8 max-md:w-4" />
 </div>
