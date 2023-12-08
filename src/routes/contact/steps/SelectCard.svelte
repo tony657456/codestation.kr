36b29d8ad45da3e60a8908a8fc0cc440d7a7cd1c
on-stu <minsu0523@naver.com>
2023-12-08 11:55:32 +0900

    Refactor card selection logic and update design section background

--- src/routes/contact/steps/SelectCard.svelte
+++ src/routes/contact/steps/SelectCard.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
 	export let number = '';
 	export let value = '';
 
-	let onClick = false;
+	export let selected = false;
+
+	const dispatch = createEventDispatcher();
 	function handleClick() {
-		onClick = !onClick;
+		dispatch('click');
 	}
 </script>
 
 <button
-	class="rounded-2xl border {onClick ? 'border-sky-400' : 'border-sky-800'}"
+	class="rounded-2xl border {selected ? 'border-sky-400' : 'border-sky-800'}"
 	on:click|preventDefault={handleClick}
 >
 	<div class="flex items-center justify-start px-2 py-1">
 		<div
-			class="rounded-full border border-sky-300 px-2 text-sm text-sky-300 {onClick
+			class="rounded-full border border-sky-300 px-2 text-sm text-sky-300 {selected
 				? 'bg-sky-500'
 				: ''}"
 		>
 			{number}
 		</div>
-		<span class="mx-2 text-lg text-sky-300 {onClick ? 'font-bold' : ''}">{value}</span>
+		<span class="mx-2 text-lg text-sky-300 {selected ? 'font-bold' : ''}">{value}</span>
 	</div>
 </button>
