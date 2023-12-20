b4d8a6c738cf38e8db653359ddb9bfe4a060658d
tony657456 <ghrn7989@daum.net>
2023-12-20 18:35:06 +0900

    chage webp

--- src/lib/constants/clean-code-board.ts
+++ src/lib/constants/clean-code-board.ts
@@ -8,31 +8,31 @@ type TClean = {
 export const CleanCodeBoard: TClean[] = [
 	{
 		index: '1',
-		imageUrl: '/images/sveltekit-icon.png',
+		imageUrl: '/images/sveltekit-icon.webp',
 		title: 'SvelteKit',
 		description: '혁신적인 풀스텍 웹 개발\n프레임워크\n압도적인 성능과 직관적 구조',
 	},
 	{
 		index: '2',
-		imageUrl: '/images/nextjs-icon.png',
+		imageUrl: '/images/nextjs-icon.webp',
 		title: 'NextJS',
 		description: 'React 기반\nSSR 프레임워크',
 	},
 	{
 		index: '3',
-		imageUrl: '/images/nest-icon.png',
+		imageUrl: '/images/nest-icon.webp',
 		title: 'NestJS',
 		description: '뛰어난 확장성의\n백엔드 프레임워크',
 	},
 	{
 		index: '4',
-		imageUrl: '/images/supabase-icon.png',
+		imageUrl: '/images/supabase-icon.webp',
 		title: 'Supabase',
 		description: '획기적인\n클라우드 데이터베이스\n파일 스토리지 솔루션',
 	},
 	{
 		index: '5',
-		imageUrl: '/images/pytorch-icon.png',
+		imageUrl: '/images/pytorch-icon.webp',
 		title: 'PyTorch',
 		description: 'AI, ML, DL을 위한\n파이썬 기반 프레임워크',
 	},
--- src/lib/constants/say-developer.ts
+++ src/lib/constants/say-developer.ts
@@ -9,6 +9,6 @@ export const SayDeveloper: TSayDeveloper[] = [
 		title: '개발자가 하고싶은 말',
 		description:
 			'하고싶은 말 하고싶은 말 하고싶은 말 하고싶은 말\n하고싶은 말 하고싶은 말 하고싶은 말 하고싶은 말',
-		imageUrl: '/images/nest-raptop.png',
+		imageUrl: '/images/nest-raptop.webp',
 	},
 ];
