24a499321ecf1a3c0fbcd8ffaaebe6aa1edcfff0
on-stu <minsu0523@naver.com>
2023-12-27 17:35:23 +0900

    FEAT : add textarea

---
+++ src/lib/components/TextArea.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let value = '';
+	export let name = '';
+</script>
+
+<textarea
+	{name}
+	bind:value
+	class="resize-none border-b-2 border-blue-600 bg-transparent p-2 text-white"
+	placeholder="답변을 입력해 주세요..."
+	rows="5"
+	cols="50"
+/>
