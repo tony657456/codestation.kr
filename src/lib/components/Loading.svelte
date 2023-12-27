2329901cd2a58cea6d05d7713f929b09633d1da1
on-stu <minsu0523@naver.com>
2023-12-27 17:35:44 +0900

    FEAT : add loading component

---
+++ src/lib/components/Loading.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Spinner from './Spinner.svelte';
+
+	export let show = false;
+	export let text = '';
+
+	onMount(() => {
+		setTimeout(() => {
+			show = true;
+		}, 500);
+
+		return () => {
+			show = false;
+		};
+	});
+</script>
+
+{#if show}
+	<div
+		tabindex="-1"
+		class="fixed left-0 right-0 top-0 z-50 flex h-screen w-full cursor-wait items-center justify-center overflow-y-auto overflow-x-hidden bg-black bg-opacity-40 transition-all md:inset-0 md:h-full"
+	>
+		<div role="status" class="flex flex-col items-center justify-center gap-2">
+			<Spinner />
+			<span class="sr-only">로딩중...</span>
+			{#if text}
+				<span class="text-center text-lg font-bold text-white">
+					{@html text}
+				</span>
+			{/if}
+		</div>
+	</div>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { navigating, page } from '$app/stores';
 	import FloatingButton from '$lib/components/FloatingButton.svelte';
+	import Loading from '$lib/components/Loading.svelte';
 	import Seo from '$lib/components/SEO.svelte';
+	import { isLoading } from '$lib/stores/loading-store';
 	import '../app.css';
 	import Footer from '../lib/components/Footer.svelte';
 	import Header from '../lib/components/Header.svelte';
 </script>
+
+{#if $navigating || $isLoading}
+	<Loading />
+{/if}
 
 <Seo />
 <Header />
