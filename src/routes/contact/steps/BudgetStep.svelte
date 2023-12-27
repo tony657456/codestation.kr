128805d035a5612f1256db448142f8735d7421e1
on-stu <minsu0523@naver.com>
2023-12-27 17:36:07 +0900

    FIX : remove isLastStep for budget

--- src/routes/contact/steps/BudgetStep.svelte
+++ src/routes/contact/steps/BudgetStep.svelte
@@ -8,7 +8,7 @@
 	let selectedBudgetType: TBudgetStep | undefined;
 </script>
 
-<EachStep bind:currentStep={step} targetStep={4} isValid isLastStep>
+<EachStep bind:currentStep={step} targetStep={4} isValid>
 	<div class="flex w-1/2 flex-col gap-4 max-md:w-full max-md:px-1">
 		<h1 class="text-3xl text-white">5. 예산을 알려주세요! 💵*</h1>
 		<div class="flex flex-col gap-3">
